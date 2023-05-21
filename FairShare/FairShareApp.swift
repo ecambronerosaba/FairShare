7124b383c86a0821d82555c7cd231af96d5839ef
Esteban Cambronero Saba <81437668+ecambronerosaba@users.noreply.github.com>
2023-05-21 16:59:24 -0700

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Esteban Cambronero on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
